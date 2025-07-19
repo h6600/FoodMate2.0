993f62621b9ecac66a09e545bc761fae5a1da2c3
Harshal Dhake <59157337+h6600@users.noreply.github.com>
2025-07-19 22:23:34 +0530

    Add files via upload

---
+++ models/cnn_model.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+
+class FoodCNN(nn.Module):
+    def __init__(self, num_classes=5):
+        super(FoodCNN, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(3, 16, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(16, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(32 * 64 * 64, 128),
+            nn.ReLU(),
+            nn.Linear(128, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(x.size(0), -1)
+        return self.fc(x)
