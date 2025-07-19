879dfbbad8909c68725266528e8e8f25f3ddb37a
h6600 <harshaldhake21@gmail.com>
2025-07-20 00:12:34 +0530

    sfgsdf

--- database/mongo_handler.py
+++ database/mongo_handler.py
@@ -2,9 +2,5 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb+srv://mongodbconnect43:mongoDB%40123@mongodemo.wndvpbd.mongodb.net/")
 db = client["FoodMate"]
-<<<<<<< HEAD
-=======
-
->>>>>>> c75102664fb183f72dee90ef03e5babf26daf090
 users = db["Users"]
 posts = db["posts"]
